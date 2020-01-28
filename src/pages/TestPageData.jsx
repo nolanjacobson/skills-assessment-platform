94861e9bffb121840611d852ed5386a2a4bac23e
Nolan Jacobson <theprophet0@users.noreply.github.com>
2020-01-28 14:57:42 -0500

    changed math rand to math ceil

--- src/pages/TestPageData.jsx
+++ src/pages/TestPageData.jsx
@@ -316,7 +316,7 @@ const TestPageData = props => {
     if (checkBox) {
       profSum.map((prof, index) => {
         setProfAverage(previous => {
-          return [...previous, Math.ceil((prof/ lengths[index] / 0.25) * 0.25).toFixed(2))]
+          return [...previous, Math.ceil((prof/ lengths[index] / 0.25) * 0.25).toFixed(2)]
         })
       })
 
